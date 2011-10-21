c5753f85387dc931218f15aede3e4c7e44059540
Florian Born <fl.born.303@gmail.com>
2011-10-21 17:05:38 +0200

    Adding missing updater source files

---
+++ updater/applicationinterface.cpp
@@ -0,0 +1,207 @@
+#include "applicationinterface.h"
+
+ApplicationInterface::ApplicationInterface(QObject *parent) :
+    QObject(parent)
+{
+    udpSocket = new QUdpSocket(this);
+    timer = new QTimer();
+    timer->setSingleShot(true);
+    timeout = new QTimer();
+    timeout->setSingleShot(true);
+    _connected = false;
+    _updateFinished = true;
+
+    connect(udpSocket, SIGNAL(readyRead()), this, SLOT(readPendingDatagrams()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(timerTrigger()));
+    connect(timeout, SIGNAL(timeout()), this, SLOT(timeoutOccured()));
+
+    if (!(udpSocket->bind(QHostAddress(HOST_ADDRESS), UPDATER_PORT)))
+        p(QString("Could not bind to %1:%2").arg(HOST_ADDRESS).arg(UPDATER_PORT));
+    else {
+        p(QString("Listening on %1:%2").arg(HOST_ADDRESS).arg(UPDATER_PORT));
+
+        timer->start(1000);
+    }
+}
+
+void ApplicationInterface::timerTrigger(void) {
+    udpSocket->writeDatagram(createCommand(PING), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+    pinging = true;
+    timeout->start(TIMEOUT_VALUE);
+}
+
+void ApplicationInterface::p(QString msg) {
+    *output << "ApplicationInterface: " << msg << endl;
+    output->flush();
+    *foutput << "ApplicationInterface: " << msg << endl;
+    foutput->flush();
+}
+
+void ApplicationInterface::timeoutOccured() {
+    if (_connected) p("Not connected");
+
+    _connected = false;
+
+    timer->start(PING_INTERVAL);
+
+    emit applicationClosed(true);
+}
+
+void ApplicationInterface::readPendingDatagrams() {
+    while (udpSocket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        QHostAddress sender;
+        quint16 senderPort;
+
+        datagram.resize(udpSocket->pendingDatagramSize());
+
+        udpSocket->readDatagram(datagram.data(), datagram.size(),
+                                &sender, &senderPort);
+
+        processCommand(datagram);
+    }
+}
+
+void ApplicationInterface::processCommand(QByteArray a) {
+    int command;
+    QByteArray payload;
+
+    command = a.at(0);
+    payload = a.mid(1);
+
+//    if (command != PONG)
+//        p("Received: "+QString::number(command)+":"+QString(payload));
+
+    switch (command) {
+    case PING:
+        udpSocket->writeDatagram(createCommand(PONG), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+        break;
+
+    case PONG:
+        if (pinging) {
+            if (!_connected) {
+                p("Connected");
+                _connected = true;
+
+                sendFailedFiles();
+            }
+
+            if (_updateFinished) {
+                udpSocket->writeDatagram(createCommand(UPDATE_FINISHED), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+            }
+
+            pinging = false;
+            timeout->stop();
+            timer->start(PING_INTERVAL);
+
+            emit applicationClosed(false);
+        }
+        else {
+            p("Received unwanted PONG");
+        }
+        break;
+
+    case CLEAR:
+        if (_connected) {
+            fu.filename = "";
+            fu.uri = "";
+            updateList.clear();
+        }
+        break;
+
+    case ADD_SET:
+        if (_connected) {
+            updateList.append(fu);
+            fu.filename = "";
+            fu.uri = "";
+        }
+        break;
+
+    case SET_TARGET:
+        if (_connected) {
+            fu.filename = QString(payload);
+        }
+        break;
+
+    case SET_URI:
+        if (_connected) {
+            fu.uri = QString(payload);
+        }
+        break;
+
+    case SET_EXE:
+        if (_connected) {
+            emit executableChanged(QString(payload));
+        }
+        break;
+
+    case START:
+        if (_connected) {
+            emit startUpdate(updateList);
+        }
+        break;
+
+    case CLOSE_REQUEST:
+        if (_connected) {
+            p("Received close request");
+            emit close();
+        }
+        break;
+
+    case DISPLAY_MSG:
+        p(QString(payload));
+        break;
+
+    case GET_VERSION:
+        udpSocket->writeDatagram(createCommand(VERSION, QString(PROGRAM_VERSION)), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+        break;
+
+    case STOP:
+    default:
+        p("Received unknown/unhandled command");
+    }
+}
+
+void ApplicationInterface::sendCloseRequest() {
+    udpSocket->writeDatagram(createCommand(CLOSE_REQUEST), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+
+    closingApplication = true;
+
+    p("Requesting Application close");
+}
+
+void ApplicationInterface::sendMessage(QString s) {
+    udpSocket->writeDatagram(createCommand(DISPLAY_MSG, s), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+}
+
+void ApplicationInterface::sendError(QString s) {
+    udpSocket->writeDatagram(createCommand(ERROR, s), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+}
+
+void ApplicationInterface::setFailedFiles(QList<FileUpdate> l) {
+    failedFiles = l;
+
+    if (_connected) sendFailedFiles();
+
+    if (failedFiles.count() > 0)
+        p("Some files failed");
+}
+
+void ApplicationInterface::sendFailedFiles() {
+    if (_connected && (failedFiles.count() > 0)) {
+        udpSocket->writeDatagram(createCommand(CLEAR), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+
+        for (int i=0; i<failedFiles.count(); i++) {
+            udpSocket->writeDatagram(createCommand(SET_URI, failedFiles.at(i).tmpFilename), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+            udpSocket->writeDatagram(createCommand(SET_TARGET, failedFiles.at(i).filename), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+            udpSocket->writeDatagram(createCommand(ADD_SET), QHostAddress(HOST_ADDRESS), APPLICATION_PORT);
+        }
+        failedFiles.clear();
+
+        p("Transmitted failed files to main application");
+    }
+}
+
+void ApplicationInterface::setUpdateFinished(bool b) {
+    _updateFinished = b;
+}
---
+++ updater/updater.cpp
@@ -0,0 +1,95 @@
+#include "updater.h"
+
+Updater::Updater(QObject *parent) :
+    QObject(parent)
+{
+    ai = new ApplicationInterface();
+    dm = new DownloadManager();
+    fh = new FileHandler();
+
+    finishedDownload = false;
+    exchanging = false;
+    ai->setUpdateFinished(false);
+}
+
+void Updater::run() {
+    connect(ai, SIGNAL(startUpdate(QList<FileUpdate>)), this, SLOT(startUpdate(QList<FileUpdate>)));
+    connect(dm, SIGNAL(downloadsFinished(QList<FileUpdate>)), this, SLOT(downloadFinished(QList<FileUpdate>)));
+    connect(ai, SIGNAL(applicationClosed(bool)), this, SLOT(startExchange(bool)));
+    connect(ai, SIGNAL(executableChanged(QString)), this, SLOT(setExecutable(QString)));
+    connect(fh, SIGNAL(exchangingFinished(bool)), this, SLOT(exchangeFinished(bool)));
+    connect(dm, SIGNAL(error(QString)), ai, SLOT(sendError(QString)));
+    connect(fh, SIGNAL(error(QString)), ai, SLOT(sendError(QString)));
+
+    connect(ai, SIGNAL(close()), this, SLOT(closeRequested()));
+}
+
+void Updater::startUpdate(QList<FileUpdate> l) {
+    updateList = l;
+    p("Starting Update");
+    finishedDownload = false;
+
+    dm->startDownload(l);
+}
+
+void Updater::downloadFinished(QList<FileUpdate> l) {
+    updateList = l;
+    finishedDownload = true;
+
+    p("Downloads finished");
+
+    ai->sendCloseRequest();
+}
+
+void Updater::startExchange(bool applicationClosed) {
+    if (applicationClosed && finishedDownload && !exchanging) {
+        p("Starting file exchange");
+        exchanging = true;
+        fh->startExchange(updateList);
+    }
+}
+
+void Updater::exchangeFinished(bool errors) {
+    p("Update process finished");
+    if (errors) {
+        ai->sendError("Update finished with errors.");
+    }
+
+    if (executable != "") {
+        p("Starting Executable file "+executable);
+        QProcess::startDetached(QString("\"%1\"").arg(executable));
+
+        ai->setFailedFiles(fh->getFailedFiles());
+    }
+    else {
+        p("Could not start application, because I don't know which one. Please start manually.");
+    }
+
+    ai->setUpdateFinished(true);
+    ai->sendMessage("Update finished.");
+}
+
+void Updater::setExecutable(QString s) {
+    QFile f;
+
+    if (f.exists(s)) {
+        executable = s;
+        p("Executable set to "+s);
+    }
+    else {
+        p("Wanted to set Executable file '"+s+"', but it does not exist.");
+        ai->sendError("Wanted to set Executable file '"+s+"', but it does not exist.");
+    }
+}
+
+
+void Updater::p(QString msg) {
+    *output << "Updater: " << msg << endl;
+    output->flush();
+    *foutput << "Updater: " << msg << endl;
+    foutput->flush();
+}
+
+void Updater::closeRequested() {
+    exit(0);
+}
